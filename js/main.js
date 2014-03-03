1bf5b66eb83e4c758e83656c3a4a4f3991619cd7
Damien <damien.khalizoff@brad.ca>
2014-03-03 14:20:37 -0500

    markers + scrollTo
    -change function add markers cluster
    - add scroll To
    - autofit (markers)

--- js/main.js
+++ js/main.js
@@ -4,42 +4,28 @@ if ( !Proto ) {
 
 Proto.gmap = {};
 Proto.Map = {};
+Proto.Map.markerList = markers;
 
 // Proto.Map.pathMarkerIcon = 'img/icons/icon-marker.png';
-
-Proto.Map.clusters = {
-  5: {
-      content: '<div class="cluster cluster-1">CLUSTER_COUNT</div>',
-      width: 53,
-      height: 52
-  },
-  20: {
-      content: '<div class="cluster cluster-2">CLUSTER_COUNT</div>',
-      width: 56,
-      height: 55
-  },
-  60: {
-      content: '<div class="cluster cluster-3">CLUSTER_COUNT</div>',
-      width: 66,
-      height: 65
-  }
-};
+Proto.Map.pathMarkerIcon = new google.maps.MarkerImage("http://maps.gstatic.com/mapfiles/icon_green.png");
 
 
 Proto.Map.addMarkersClustering = function( markerList, markerIcon, clusters ) {
 
   Proto.gmap.gmap3( {
     action: 'addMarkers',
-    radius: 75,
-    markers: markerList,
-    clusters: clusters,
+    // radius: 75,
+    // markers: markerList,
+    // clusters: clusters,
     marker: {
+      values: markerList,
       options: {
           icon: markerIcon
       },
       events: {
         click: function( marker, event, data ) {
-          // $( '.js-scroll' ).scrollTo( '.js-article-' + data.id , 200 );
+          $( '.js-scroll' ).scrollTo( '.js-marker-' + data.data.id , 200 );
+          console.log(data.data.id);
           // var content = Feis.Map.setContent( data );
           // var map = $( this ).gmap3( 'get' );
           // infowindow = $( this ).gmap3( { action:'get' , name:'infowindow' } );
@@ -74,8 +60,35 @@ Proto.Map.addMarkersClustering = function( markerList, markerIcon, clusters ) {
           //   } );
           // }
         }
+      },
+      cluster:{
+        radius: 100,
+        events:{
+          mouseover: function(cluster){
+            $(cluster.main.getDOMElement()).css("border", "1px solid red");
+          },
+          mouseout: function(cluster){
+            $(cluster.main.getDOMElement()).css("border", "0px");
+          }
+        },
+        0: {
+          content: "<div class='cluster cluster-1'>CLUSTER_COUNT</div>",
+          width: 53,
+          height: 52
+        },
+        20: {
+          content: "<div class='cluster cluster-2'>CLUSTER_COUNT</div>",
+          width: 56,
+          height: 55
+        },
+        50: {
+          content: "<div class='cluster cluster-3'>CLUSTER_COUNT</div>",
+          width: 66,
+          height: 65
+        }
       }
     },
+    autofit: {}
   } );
 
 };
@@ -107,6 +120,7 @@ Proto.Map.getMarkersFromDatabase = function( zoomLevel ) {
       }
     }
   }
+  console.log(result);
   return result;
 };
 
@@ -116,11 +130,17 @@ Proto.Map.appendMarkers = function( zoomLevel ) {
     clear:{name:"clusterer"},
     marker:{
       values: Proto.Map.getMarkersFromDatabase(zoomLevel),
+      events: {
+        click: function( marker, event, data ) {
+          console.log( data );
+          // $( '.js-scroll' ).scrollTo( '.js-article-' + data.id , 200 );
+        }
+      },
       cluster:{
         radius:100,
         calculator: function(values){
           var i, cnt = 0;
-          for(i=0; i<values.length; i++){
+          for( i=0; i<values.length; i++){
             if (values[i] && values[i].data && values[i].data.count){
               cnt += values[i].data.count;
             } else {
@@ -152,6 +172,11 @@ Proto.Map.appendMarkers = function( zoomLevel ) {
   });
 }
 
+
+Proto.Map.initMarkers = function() {
+
+  Proto.Map.addMarkersClustering( Proto.Map.markerList, Proto.Map.pathMarkerIcon , Proto.Map.clusters );
+}
 
 //Clear
 Proto.Map.clearThat = function() {
@@ -199,15 +224,16 @@ Proto.Map.init = function( ) {
     },
     events:{
       zoom_changed: function( map ) {
-        Proto.Map.appendMarkers( map.getZoom() );
+        // Proto.Map.appendMarkers( map.getZoom() );
       }
     },
       callback: function( map ) {
-        Proto.Map.appendMarkers( map.getZoom() );
+        // Proto.Map.appendMarkers( map.getZoom() );
       }
     }
 
   } );
+  Proto.Map.initMarkers();
 
   $( '.js-default' ).bind( 'click', Proto.Map.init );
   $( '.js-clear' ).bind( 'click', Proto.Map.clearThat );
